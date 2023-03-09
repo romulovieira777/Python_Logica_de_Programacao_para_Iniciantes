87e239ee57177aa1acdb46ab35289c5c4a152360
Romulo Vieira <romulo.vieira777@gmail.com>
2023-03-09 18:05:49 -0300

    Tipos de Dados Numéricos em Python

---
+++ Secao_02_Tipos_de_Dados_Numericos_em_Python/03_Numeros_Flutuantes.py
@@ -0,0 +1,8 @@
+a = 10.37
+b = 7.75
+c = a * b
+print(c)
+print(type(c))
+
+resultado = "Resultado: {:7.4f}".format(c)
+print(resultado)
---
+++ Secao_02_Tipos_de_Dados_Numericos_em_Python/04_Numeros_Complexos.py
@@ -0,0 +1,16 @@
+a = 2 + 4j
+b = 6 + 3j
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a ** b
+
+print(type(a))
+print(type(b))
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
