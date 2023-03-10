3afa503654d5abca1ffe35bd7fb815988c2e23f1
Romulo Vieira <romulo.vieira777@gmail.com>
2023-03-10 12:33:32 -0300

    Tipos de Dados Numéricos em Python

---
+++ Secao_02_Tipos_de_Dados_Numericos_em_Python/Codigo_Python/08_Comparar_Valores.py
@@ -0,0 +1,17 @@
+a = 10
+b = 11
+
+c = a < b
+d = a > b
+e = a != b
+f = a == b
+g = a >= b
+h = a <= b
+
+print(type(c))
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
---
+++ Secao_02_Tipos_de_Dados_Numericos_em_Python/Codigo_Python/09_Operadores_Logicos.py
@@ -0,0 +1,18 @@
+# Operadores Lógicos
+# and - or - not
+
+a = 10
+b = 20
+
+c = (a and b) > 20
+d = (a or b) > 20
+e = not (a and b) > 20
+f = (not a)
+g = not(not(a))
+
+print(type(c))
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
